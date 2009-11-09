65dc3ad48c2e3e16e7712d65e2611b3bdd9f9a8d
Whiteknight <wknight8111@gmail.com>
2009-11-09 11:51:49 -0500

    Update URL, prettify code

--- BookDesigner.php
+++ BookDesigner.php
@@ -9,18 +9,18 @@ EOT;
 }
  
 $wgExtensionCredits['specialpage'][] = array(
-	'name' => 'BookDesigner',
-	'author' => 'Andrew Whitworth',
-	'url' => 'http://www.wittiewiki.org',
-	'description' => 'Design and create Books',
+	'name'           => 'BookDesigner',
+	'author'         => 'Andrew Whitworth',
+	'url'            => 'http://github.com/Whiteknight/mediawiki-bookdesigner',
+	'description'    => 'Design and create Books',
 	'descriptionmsg' => 'BookDesigner-desc',
-	'version' => '0.0.0',
+	'version'        => '0.0.1',
 );
  
 $dir = dirname(__FILE__) . '/';
  
-$wgAutoloadClasses['BookDesigner'] = $dir . 'BookDesigner_body.php'; # Tell MediaWiki to load the extension body.
+$wgAutoloadClasses['BookDesigner']        = $dir . 'BookDesigner_body.php'; 
 $wgExtensionMessagesFiles['BookDesigner'] = $dir . 'BookDesigner.i18n.php';
-$wgExtensionAliasesFiles['BookDesigner'] = $dir . 'BookDesigner.alias.php';
-$wgSpecialPages['BookDesigner'] = 'BookDesigner'; # Let MediaWiki know about your new special page.
+$wgExtensionAliasesFiles['BookDesigner']  = $dir . 'BookDesigner.alias.php';
+$wgSpecialPages['BookDesigner']           = 'BookDesigner'; 
 
