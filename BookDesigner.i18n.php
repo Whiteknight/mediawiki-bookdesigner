5eacf5c8fe2de89f969d1d05c417c6a041b1281a
Whiteknight <wknight8111@gmail.com>
2010-01-11 09:26:32 -0500

    add a space and a TODO note about future i18n efforts

--- BookDesigner.i18n.php
+++ BookDesigner.i18n.php
@@ -1,9 +1,17 @@
 <?php
 $messages = array();
 
+/* TODO BIG PROJECT:
+Add proper i18n support for the UI. This is going to be a big project.
+For the javascript, we will probably need to generate a string table
+in the PHP backend and transmit that to the client. The JS will then
+read string constants from this table to generate the UI. For the rest
+of the UI, we can insert string constants directly.
+*/
+// TODO: Can we get access to the raw $messages array from BookDesigner_body.php?
 /* *** English *** */
 $messages['en'] = array(
-	'bookdesigner' => 'BookDesigner',
+	'bookdesigner' => 'Book Designer',
 	'bookdesigner-desc' => "Design and create books",
 );
 
